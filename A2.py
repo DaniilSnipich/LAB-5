2080423ca01cb4d91d9360a1d7053bc90c30ba4b
Danik <momentssd@gmail.com>
2025-11-17 19:09:36 +0300

    Update A2.py
    Создал функцию строки у котого 1 параметр items
    создал цикл for который будет проходиться по каждому элементу в items
    ну и выводим его

--- A2.py
+++ A2.py
@@ -2,3 +2,9 @@ import re
 
 text = 'He jests at scars. That never felt a wound!   Hello, friend!   Are you OK?'
 sen = re.split(r'(?<=[.?!]) +', text)
+
+def stroki (items):
+    for item in items:
+        print(item)
+
+
