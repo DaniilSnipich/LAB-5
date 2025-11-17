cdfbd062cd2f1ed1fc6627a853b0905a7c0e4a1f
Danik <momentssd@gmail.com>
2025-11-17 19:11:16 +0300

    Update A2.py

--- A2.py
+++ A2.py
@@ -1,12 +1,13 @@
 import re
 
 text = 'He jests at scars. That never felt a wound!   Hello, friend!   Are you OK?'
-sen = re.split(r'(?<=[.?!]) +', text)
+sentences = re.split(r'(?<=[.?!]) +', text)
 
 def stroki (items):
     for item in items:
         print(item)
 
-stroki(sen)
+stroki(sentences)
 print(f'Предложений в тексте: {len(sen)}')
+
 
