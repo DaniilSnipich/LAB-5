934a7cd0de8640cf1b5b91cf05031be134ec392f
Danik <momentssd@gmail.com>
2025-11-17 19:16:50 +0300

    Update A1.py
    добавил условие которые определяет была ли найдена скобка, если да то убирает от ( до )

--- A1.py
+++ A1.py
@@ -3,3 +3,6 @@ text = str(input())
 while '(' in text:
         position1 = text.find('(')
         position2 = text.find(')', position1)
+
+if position2 != 1:
+            text = text.replace(text[position1:position2 + 1], '')
